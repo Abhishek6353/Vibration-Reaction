fcb4a8307eaeb3e3ea920205cbbfc30a5422fe04
Abhishek Kurmi <itsabhi814@gmail.com>
2025-12-17 18:23:45 +0530

    Initial Commit

---
+++ Vibration ReactionDemo/Vibration_ReactionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vibration_ReactionDemoApp.swift
+//  Vibration ReactionDemo
+//
+//  Created by Apple on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Vibration_ReactionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
